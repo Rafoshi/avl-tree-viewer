5bb9898ca26480be6f199ea8c8e9e4e85c9c16b0
rafoshi <rafa.ioshi@gmail.com>
2023-09-21 00:27:58 -0300

    .

--- dest/app.js
+++ dest/app.js
@@ -6,21 +6,9 @@ root = tree.insert(root, 43);
 root = tree.insert(root, 10);
 root = tree.insert(root, 21);
 root = tree.insert(root, 66);
-//          32
-//          /\
-//      10      43
-//        \      \
-//       21      66
-tree.inOrder(root);
 root = tree.delete(root, 32);
 root = tree.delete(root, 66);
-//          43
-//          /\
-//      10     66
-//       \
-//       21
 tree.inOrder(root);
-//          43
-//          /\
-//       32    76
+//Draw the tree
+//draw(root);
 console.log(root);
